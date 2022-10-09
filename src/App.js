93b9af326beeaa47fbae54ee3c25587cbd5657cc
quangngonz <quangngo.nz@gmail.com>
2022-10-09 22:36:48 +0700

    innit camera

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Webcam from "react-webcam";
 
 function App() {
+  const videoConstraints = {
+    facingMode: "user",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Webcam videoConstraints={videoConstraints} />
+      <h1>Người gart cần tìm trên là bạn!</h1>
     </div>
   );
 }
